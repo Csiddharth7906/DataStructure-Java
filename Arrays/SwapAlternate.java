84276b1fd0b5249068ee3919dde162a94e71d25d
Siddharth Chauhan <siddharthchauhan7906@gmail.com>
2025-08-24 17:27:09 +0530

    my Code is working yeahhhhhhhh

---
+++ Arrays/SwapAlternate.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class SwapAlternate{
+    public static int[] takeInput(){
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int [] input = new int[size];
+        for(int i=0;i<size;i++){
+            input[i]=sc.nextInt();
+        }
+        return input;
+
+    }
+    public  static void print(int input[]){
+        int size=input.length;
+        for(int i=0;i<size;i++){
+            System.out.print(input[i]+" ");
+        }
+        System.out.println();
+    }
+         public static void main(String args[]){
+          int arr[]=takeInput();
+          for(int i=0;i<arr.length-1;i++){
+              int temp=arr[i];
+              arr[i]=arr[i+1];
+              arr[i+1]=temp;
+              i++;
+          }
+          print(arr);
+
+         }
+}
