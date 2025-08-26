c4a01f8e2e7b0bc7dbcf28e13712bd2650470bf7
Siddharth Chauhan <siddharthchauhan7906@gmail.com>
2025-08-26 18:19:37 +0530

    did sort

--- Arrays/InsertionSort.java
+++ Arrays/InsertionSort.java
@@ -1,7 +1,7 @@
 public class InsertionSort {
     public static void Sorting(int []arr){
-        for (int i = 0; i < arr.length; i++) {
-            for(int i)
+        for (int i = 1; i < arr.length; i++) {
+          int j=i-1
         }
     }
     public static void main(String[] args) {
